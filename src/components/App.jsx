3376261a0eb5a58b67f0bf825015c67fadd2c3de
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-08-10 21:24:07 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,20 @@ import Filter from './Filter/Filter';
 
 export default function App() {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('Contacts'));
+    return ( JSON.parse(localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+    );
   });
   const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
 
   function onSubmitForm({ name, number }) {
     const contact = { name, number, id: nanoid() };
@@ -37,9 +48,6 @@ export default function App() {
     );
   };
 
-  useEffect(() => {
-    localStorage.setItem('Contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   return (
     <div>
